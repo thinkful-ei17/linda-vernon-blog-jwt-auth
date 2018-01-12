2314e15d5a7965f57fe1f7b9164855064b442c70
Vernon Mensah <vernonmensah@gmail.com>
2018-01-12 14:52:12 -0500

    Added blog router and deleted other paths.

---
+++ router/blogRouter.js
@@ -0,0 +1,188 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const blogRouter = express.Router();
+
+const { BlogPost, UserInfo } = require('../models');
+
+blogRouter.get('/posts', (req, res) => {
+    BlogPost
+        .find()
+        .then(posts => {
+            res.json(posts.map(post => post.serialize()));
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went terribly wrong' });
+        });
+});
+
+blogRouter.get('/posts/:id', (req, res) => {
+    BlogPost
+        .findById(req.params.id)
+        .then(post => res.json(post.serialize()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went horribly awry' });
+        });
+});
+
+blogRouter.post('/posts', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+
+    BlogPost
+        .create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        })
+        .then(blogPost => res.status(201).json(blogPost.serialize()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Something went wrong' });
+        });
+
+});
+
+
+blogRouter.delete('/posts/:id', (req, res) => {
+    BlogPost
+        .findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).json({ message: 'success' });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went terribly wrong' });
+        });
+});
+
+
+blogRouter.put('/posts/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        res.status(400).json({
+            error: 'Request path id and request body id values must match'
+        });
+    }
+
+    const updated = {};
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
+
+    BlogPost
+        .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+        .then(updatedPost => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+});
+
+
+blogRouter.post('/users', function (req, res) {
+    const requiredFields = ['username','firstName','lastName','password'];
+
+    const missingField = requiredFields.find(field =>!(field in req.body));
+
+    const nonStringField = requiredFields.find(
+        field => field in req.body && typeof req.body[field] !== 'string');
+
+    if (missingField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: 'Missing field',
+            location: missingField
+        });
+    }
+    if (nonStringField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: 'Incorrect field type: expected string',
+            location: nonStringField
+        });
+    }
+
+    const sizedFields = {
+        username: {
+            min: 1
+        },
+        password: {
+            min: 8,
+            // bcrypt truncates after 72 characters, so let's not give the illusion
+            // of security by storing extra (unused) info
+            max: 72
+        }
+    };
+    const tooSmallField = Object.keys(sizedFields).find(
+        field =>
+            'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+    );
+    const tooLargeField = Object.keys(sizedFields).find(
+        field =>
+            'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+    );
+
+    if (tooSmallField || tooLargeField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: tooSmallField
+                ? `Must be at least ${sizedFields[tooSmallField]
+                    .min} characters long`
+                : `Must be at most ${sizedFields[tooLargeField]
+                    .max} characters long`,
+            location: tooSmallField || tooLargeField
+        });
+    }
+
+    const {firstName, lastName, username, password} = req.body;
+
+    return UserInfo.find({username})
+        .count()
+        .then(count => {
+            if(count > 0) {
+                return Promise.reject({
+                    code: 422,
+                    reason: 'ValidationError',
+                    message: 'Username already taken',
+                    location: 'username'
+                });
+            }
+
+            return UserInfo.hashPassword(password);
+        })
+        .then(digest => {
+            return UserInfo.create({
+                username,
+                password: digest,
+                firstName,
+                lastName
+            });
+        })
+        .then(user => {  //not using nodemon to watch changes keep eye on log!
+            return res.status(201).location(`/users/${user.id}`).json(user.serialize());
+        })
+        .catch(err => {
+            if (err.reason === 'ValidationError') {
+                return res.status(err.code).json(err);
+            }
+            res.status(500).json({code: 500, message: 'Internal server error'});
+        });
+});
+
+module.exports = { blogRouter };
--- server.js
+++ server.js
@@ -14,13 +14,15 @@ const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const { JWT_SECRET } = require('./config');
 const { JWT_EXPIRY } = require('./config');
 
+
 const { DATABASE_URL, PORT } = require('./config');
 const { BlogPost, UserInfo } = require('./models');
-
+const { blogRouter } = require('./router/blogRouter');
 const app = express();
 
 app.use(morgan('common'));
 app.use(bodyParser.json());
+app.use('/api/', blogRouter );
 
 //=====ROUTER=====//
 const createAuthToken = function(user) {
@@ -31,7 +33,7 @@ const createAuthToken = function(user) {
     });
 };
 
-const jwtAuth = passport.authenticate('jwt', {session: false});
+
 
 // ===== Define and create basicStrategy =====
 const localStrategy = new LocalStrategy((username, password, done) => {
@@ -39,16 +41,16 @@ const localStrategy = new LocalStrategy((username, password, done) => {
     UserInfo
         .findOne({ username })
         .then(results => {
-            user = results;    
-      
+            user = results;
+
             if (!user) {
                 return Promise.reject({
                     reason: 'LoginError',
                     message: 'Incorrect username',
                     location: 'username'
                 });
-            }    
-    
+            }
+
             return user.validatePassword(password);
         })
         .then(isValid => {
@@ -59,7 +61,7 @@ const localStrategy = new LocalStrategy((username, password, done) => {
                     location: 'password'
                 });
             }
-            return done(null, user);    
+            return done(null, user);
         })
         .catch(err => {
             if (err.reason === 'LoginError') {
@@ -71,11 +73,7 @@ const localStrategy = new LocalStrategy((username, password, done) => {
         });
 });
 
-passport.use(localStrategy);
-
-const localAuth = passport.authenticate('local', { session: false });
-
-const jwtStrategy = new JwtStrategy( 
+const jwtStrategy = new JwtStrategy(
     {
         secretOrKey: JWT_SECRET,
         // Look for the JWT as a Bearer auth header
@@ -88,8 +86,11 @@ const jwtStrategy = new JwtStrategy(
     }
 );
 
+passport.use(localStrategy);
 passport.use(jwtStrategy);
 
+const jwtAuth = passport.authenticate('jwt', {session: false});
+const localAuth = passport.authenticate('local', { session: false });
 app.post('/login', localAuth, (req, res) => {
     console.log(JWT_SECRET);
     const authToken = createAuthToken(req.user.serialize());
@@ -100,200 +101,6 @@ app.post('/refresh', jwtAuth, (req, res) => {
     const authToken = createAuthToken(req.user);
     res.json({authToken});
 });
-
-app.get('/posts', (req, res) => {
-    BlogPost
-        .find()
-        .then(posts => {
-            res.json(posts.map(post => post.serialize()));
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'something went terribly wrong' });
-        });
-});
-
-app.get('/posts/:id', (req, res) => {
-    BlogPost
-        .findById(req.params.id)
-        .then(post => res.json(post.serialize()))
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'something went horribly awry' });
-        });
-});
-
-app.post('/posts', (req, res) => {
-    const requiredFields = ['title', 'content', 'author'];
-    for (let i = 0; i < requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`;
-            console.error(message);
-            return res.status(400).send(message);
-        }
-    }
-
-    BlogPost
-        .create({
-            title: req.body.title,
-            content: req.body.content,
-            author: req.body.author
-        })
-        .then(blogPost => res.status(201).json(blogPost.serialize()))
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'Something went wrong' });
-        });
-
-});
-
-
-app.delete('/posts/:id', (req, res) => {
-    BlogPost
-        .findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.status(204).json({ message: 'success' });
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'something went terribly wrong' });
-        });
-});
-
-
-app.put('/posts/:id', (req, res) => {
-    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-        res.status(400).json({
-            error: 'Request path id and request body id values must match'
-        });
-    }
-
-    const updated = {};
-    const updateableFields = ['title', 'content', 'author'];
-    updateableFields.forEach(field => {
-        if (field in req.body) {
-            updated[field] = req.body[field];
-        }
-    });
-
-    BlogPost
-        .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-        .then(updatedPost => res.status(204).end())
-        .catch(err => res.status(500).json({ message: 'Something went wrong' }));
-});
-
-
-app.delete('/:id', (req, res) => {
-    BlogPost
-        .findByIdAndRemove(req.params.id)
-        .then(() => {
-            console.log(`Deleted blog post with id \`${req.params.ID}\``);
-            res.status(204).end();
-        });
-});
-
-app.post('/users', function (req, res) {
-    const requiredFields = ['username','firstName','lastName','password'];
-
-    const missingField = requiredFields.find(field =>!(field in req.body));
-
-    const nonStringField = requiredFields.find(
-        field => field in req.body && typeof req.body[field] !== 'string');
-    
-    if (missingField) {
-        return res.status(422).json({
-            code: 422,
-            reason: 'ValidationError',
-            message: 'Missing field',
-            location: missingField
-        });
-    }
-    if (nonStringField) {
-        return res.status(422).json({
-            code: 422,
-            reason: 'ValidationError',
-            message: 'Incorrect field type: expected string',
-            location: nonStringField
-        });
-    }
-  
-    const sizedFields = {
-        username: {
-            min: 1
-        },
-        password: {
-            min: 8,
-            // bcrypt truncates after 72 characters, so let's not give the illusion
-            // of security by storing extra (unused) info
-            max: 72
-        }
-    };
-    const tooSmallField = Object.keys(sizedFields).find(
-        field =>
-            'min' in sizedFields[field] &&
-            req.body[field].trim().length < sizedFields[field].min
-    );
-    const tooLargeField = Object.keys(sizedFields).find(
-        field =>
-            'max' in sizedFields[field] &&
-            req.body[field].trim().length > sizedFields[field].max
-    );
-
-    if (tooSmallField || tooLargeField) {
-        return res.status(422).json({
-            code: 422,
-            reason: 'ValidationError',
-            message: tooSmallField
-                ? `Must be at least ${sizedFields[tooSmallField]
-                    .min} characters long`
-                : `Must be at most ${sizedFields[tooLargeField]
-                    .max} characters long`,
-            location: tooSmallField || tooLargeField
-        });
-    }
-
-    const {firstName, lastName, username, password} = req.body;
-
-    return UserInfo.find({username})
-        .count()
-        .then(count => {
-            if(count > 0) {
-                return Promise.reject({
-                    code: 422, 
-                    reason: 'ValidationError',
-                    message: 'Username already taken',
-                    location: 'username'
-                });
-            }
-
-            return UserInfo.hashPassword(password);  
-        })
-        .then(digest => {
-            return UserInfo.create({
-                username,
-                password: digest,
-                firstName,
-                lastName
-            });
-        })
-        .then(user => {  //not using nodemon to watch changes keep eye on log!
-            return res.status(201).location(`/users/${user.id}`).json(user.serialize());
-        }) 
-        .catch(err => {
-            if (err.reason === 'ValidationError') {
-                return res.status(err.code).json(err);
-            }
-            res.status(500).json({code: 500, message: 'Internal server error'});
-        });
-});
-
-// ===== Protected endpoint =====
-app.post('/login', localAuth, function (req, res) {
-    console.log(`${req.user.username} successfully logged in.`);
-    return res.json({ data: 'Top Secret' });
-}); 
-
 
 app.use('*', function (req, res) {
     res.status(404).json({ message: 'Not Found' });
@@ -346,4 +153,3 @@ if (require.main === module) {
 }
 
 module.exports = { runServer, app, closeServer, localStrategy, jwtStrategy };
-
